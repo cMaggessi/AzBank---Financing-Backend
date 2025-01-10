89c387f800bd6f120b48976385ed1dc4b564150c
cMaggessi <oneyhh@gmail.com>
2025-01-10 10:52:37 -0300

    init commit

---
+++ src/main/java/com/cmaggessi/azbank/AzBankApplication.java
@@ -0,0 +1,13 @@
+package com.cmaggessi.azbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzBankApplication.class, args);
+	}
+
+}
